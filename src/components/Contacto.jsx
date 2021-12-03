733ed55b9c5fc317964d42d4991c7d2846ed9747
Ma77i <mattiasuru@gmail.com>
2021-12-03 15:48:00 -0300

    agregado submit form y numero de tel

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,12 +1,17 @@
+import { useRef } from 'react'
+
+
 const Contacto = () => {
-/*
-    document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    
+    const contactForm = useRef(null)
+    
+    const handleSubmit = e => {
         e.preventDefault();
         e.target.elements.name.value = '';
         e.target.elements.email.value = '';
         e.target.elements.message.value = '';
-      });
-*/
+    };
+
 
     return (
         <div className="contacto">
@@ -14,10 +19,27 @@ const Contacto = () => {
 
             <section id="contact">
                 <div className="contact-wrapper">
-                    <form id="contact-form" className="form-horizontal">
-                        <input type="text" className="form-control" id="name" placeholder="Nombre" name="name" required />
-                        <input type="email" className="form-control" id="email" placeholder="Email" name="email" required />
-                        <textarea className="form-control" rows="6" placeholder="Mensaje" name="message" required />
+                    <form ref={contactForm} onSubmit={handleSubmit} className="form-horizontal">
+                        <input 
+                            type="text" 
+                            className="form-control" 
+                            id="name" 
+                            placeholder="Nombre" 
+                            name="name" required 
+                        />
+                        <input 
+                            type="email" 
+                            className="form-control" 
+                            id="email" 
+                            placeholder="Email" 
+                            name="email" required 
+                        />
+                        <textarea 
+                            className="form-control" 
+                            rows="6" 
+                            placeholder="Mensaje" 
+                            name="message" required 
+                        />
                         <button className="send-button" id="submit" type="submit" value="SEND">ENVIAR</button>
                     </form>
                     
@@ -31,7 +53,7 @@ const Contacto = () => {
                         <li className="list-item">
                             <i className="fa fa-phone fa-2x">
                             <span className="contact-text phone">
-                                <a href="tel:1-212-555-5555" title="Give me a call">(+598) 99 063 761</a>
+                                <a href="tel:+598 99 063 761" title="Give me a call">(+598) 99 063 761</a>
                             </span>
                             </i>
                         </li>
